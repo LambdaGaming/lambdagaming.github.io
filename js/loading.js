c8df3df1973fcda9efa2fabcc327b485da2c519e
LambdaGaming <starwars18m@gmail.com>
2019-12-11 19:55:14 -0500

    Added info boxes to loading screen

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,11 @@
 
 function WriteImage(){
 	var image = document.getElementById( "image" )
+	var initimage = Math.floor( Math.random() * images.length ) // Initialize the image with a random one so the page isn't blank for 3 seconds
+	image.src = images[initimage]
 	function RandomImage(){
 		var randimage = Math.floor( Math.random() * images.length )
-		image.src = images[randimage]
+		image.src = images[randimage] // Set image to a random one from the list
 	}
-	setInterval( RandomImage, 3000 )
+	setInterval( RandomImage, 3000 ) // 3 second timer loop
 }
