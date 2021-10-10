c5872dec8bfd55daa66ec31bcd070179a65cdcf6
LambdaGaming <starwars18m@gmail.com>
2021-10-09 20:59:24 -0400

    Navbar logo size fixes

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,11 @@
 window.onscroll = () => {
 	const nav = document.querySelector( "#navbar" )
-	const logo = document.getElementById( "logo" )
 	const dropdown = document.getElementById( "dropdown" )
 	const navid = document.getElementById( "navbar" )
 	const menu = document.getElementById( "menu" )
 	if ( this.scrollY <= 50 )
 	{
 		nav.className = ""
-		logo.style.width = "5%"
 		dropdown.style.backgroundColor = "black"
 		navid.style.height = "10%"
 		menu.style.top = "27%"
@@ -15,7 +13,6 @@ window.onscroll = () => {
 	else
 	{
 		nav.className = "scroll"
-		logo.style.width = "3%"
 		dropdown.style.backgroundColor = "#000011"
 		navid.style.height = "5%"
 		menu.style.top = "0%"
