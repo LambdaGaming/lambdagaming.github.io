3b7b39afcdd3cca56abde44ffe833da82f6af4ea
LambdaGaming <starwars18m@gmail.com>
2022-04-27 00:03:03 -0400

    Use json properly

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,9 @@
 function GetScreenshots() {
-	for ( i = 0; i < data.length; i++ ) {
+	const json = fetch( "../data/screenshots.json" ).then( response => response.json() )
+	var gallery = document.getElementById( "gallery" )
+	for ( const pic of json ) {
 		var figure = document.createElement( "figure" ) 
-		figure.innerHTML = '<img src="' + data[i].src + '" loading="lazy"> <figcaption>' + data[i].caption + '<figcaption>'
-		var gallery = document.getElementById( "gallery" )
+		figure.innerHTML = `<img src="${pic.src} loading="lazy"><figcaption>${pic.caption}<figcaption>`
 		gallery.append( figure )
 	}
 }
