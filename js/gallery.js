df567c6c3c86321e7e159a2a073fc176910279f7
LambdaGaming <starwars18m@gmail.com>
2022-04-27 01:55:13 -0400

    Gallery fix

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,12 @@
 function GetScreenshots() {
-	const json = fetch( "../data/screenshots.json" ).then( response => response.json() )
-	var gallery = document.getElementById( "gallery" )
-	for ( const pic of json ) {
-		var figure = document.createElement( "figure" ) 
-		figure.innerHTML = `<img src="${pic.src} loading="lazy"><figcaption>${pic.caption}<figcaption>`
-		gallery.append( figure )
-	}
+	fetch( "../data/screenshots.json" )
+		.then( response => response.json() )
+		.then( json => {
+			var gallery = document.getElementById( "gallery" )
+			for ( const pic of json.data ) {
+				var figure = document.createElement( "figure" ) 
+				figure.innerHTML = `<img src="${pic.src}" loading="lazy"><figcaption>${pic.caption}<figcaption>`
+				gallery.append( figure )
+			}
+		} )
 }
