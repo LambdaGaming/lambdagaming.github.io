2fa1a02d8bd21ab4b5d85831d4a79f4f51b646ed
LambdaGaming <starwars18m@gmail.com>
2022-05-15 21:12:18 -0400

    Fix loading screen screenshots still not working

--- js/loading.js
+++ js/loading.js
@@ -1,40 +1,38 @@
 function WriteImage() {
-	var id = ""
-	var images = []
-	var data
 	fetch( "../data/screenshots.json" )
 		.then( response => response.json() )
 		.then( json => {
-			data = json.data
+			var id = ""
+			var images = []
+			
+			switch ( imageid ) {
+				case 1:
+					id = "cityrp"
+					break
+				case 2:
+					id = "hlurp"
+					break
+				case 3:
+					id = "various"
+					break
+				case 4:
+					id = "sandbox"
+					break
+			}
+		
+			for ( const pic of json.data ) {
+				if ( pic.id == id ) {
+					images.push( pic.src )
+				}
+			}
+		
+			var image = document.getElementById( "image" )
+			var initimage = Math.floor( Math.random() * images.length ) // Initialize the image with a random one so the page isn't blank for 3 seconds
+			image.src = images[initimage]
+			function RandomImage() {
+				var randimage = Math.floor( Math.random() * images.length )
+				image.src = images[randimage] // Set image to a random one from the list
+			}
+			setInterval( RandomImage, 3000 ) // 3 second timer loop
 		} )
-
-	switch ( imageid ) {
-		case 1:
-			id = "cityrp"
-			break
-		case 2:
-			id = "hlurp"
-			break
-		case 3:
-			id = "various"
-			break
-		case 4:
-			id = "sandbox"
-			break
-	}
-
-	for ( const pic of data ) {
-		if ( pic.id == id ) {
-			images.push( pic.src )
-		}
-	}
-
-	var image = document.getElementById( "image" )
-	var initimage = Math.floor( Math.random() * images.length ) // Initialize the image with a random one so the page isn't blank for 3 seconds
-	image.src = images[initimage]
-	function RandomImage() {
-		var randimage = Math.floor( Math.random() * images.length )
-		image.src = images[randimage] // Set image to a random one from the list
-	}
-	setInterval( RandomImage, 3000 ) // 3 second timer loop
 }
