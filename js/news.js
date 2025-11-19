39fadc7bc665548a44579b24923223644f3d1ee4
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-11-18 20:32:25 -0500

    Add news page

---
+++ js/news.js
@@ -0,0 +1,42 @@
+let news = []
+
+function LoadNews() {
+	fetch( "../data/news.json" )
+		.then( response => response.json() )
+		.then( json => {
+			news = json;
+		} );
+}
+
+function ShowNews( id ) {
+	var currentId = news.length;
+	var container = document.getElementById( "newsContainer" );
+	container.innerHTML = "<h1>News</h1><hr>";
+	for ( const n of news ) {
+		if ( id != null && Number( id ) != currentId ) {
+			currentId--;
+			continue;
+		}
+		var div = document.createElement( "div" );
+		var link = document.createElement( "a" );
+		var title = document.createElement( "h2" );
+		var date = document.createElement( "h4" );
+		var author = n.author || "OPGman";
+		link.href = `https://lambdagaming.github.io/news?id=${currentId}`;
+		title.innerHTML = n.title;
+		title.id = String( currentId );
+		date.innerHTML = `Published on ${n.date} by ${author}`;
+		link.append( title );
+		div.append( link, date );
+		div.innerHTML += n.info + "<hr>";
+		container.append( div );
+		currentId--;
+	}
+}
+
+document.addEventListener( "DOMContentLoaded", function() {
+	LoadNews();
+	const params = new URL( window.location.href ).searchParams;
+	var id = params.get( "id" );
+	ShowNews( id );
+} )
